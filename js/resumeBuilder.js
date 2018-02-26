3815106b2bac1d00d5e921bf4776c19f9d40431b
MinaSamir <menasamercap@gmail.com>
2018-02-26 02:46:39 +0200

    lesson 3 video 2

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,9 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+  $("#main").append(["MinaSamir"]);
+  console.log(299792458*1000/1000000000);
+
+  var awesomeThoughts = "Iam MinaSamir and Iam AWESOME";
+  console.log(awesomeThoughts);
